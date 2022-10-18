6f987a885c08ae75318dd7fdb5e86bfb56f14c58
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-10-18 01:31:02 -0700

    Empty

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import About from "./Components/About/About.js";
 import Admin from "./Components/Admin/Admin.js";
 import Header from "./Components/Navbars/Header.js";
 import Contact from './Components/Contact/Contact.js'
-import Footer from './Components/Footer.js'
-import React from "react";
+// import Footer from './Components/Footer.js'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideNavbar from "./Components/Navbars/SideNavbar.js";
-function App() {
+import UserInputForm from "./Components/InputForm/UserInputForm";
+function App(props) {
+  const [FormIsShown, setFormIsShown] = useState(false)
+  const ShowFormFun=()=>{
+    setFormIsShown(true);
+  }
+  const HideFormFun=()=>{
+    setFormIsShown(false);
+  }
+  
   return (
     <React.Fragment>
+     { FormIsShown && <UserInputForm onClose={HideFormFun}/>}
       <BrowserRouter>
+
         <div className={clasess.container}>
           <div className={clasess.item1}>
             <Header />
@@ -21,14 +32,14 @@ function App() {
           </div>
           <div className={clasess.item3}>
           <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home OnClick={ShowFormFun}/>} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
           </div>
         </div>
-        <Footer/>
+        {/* <Footer/> */}
         
       </BrowserRouter>
     </React.Fragment>
--- src/Components/UI/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Button.module.css'
-export default function Button() {
-    return (
-        <div   >
-            <button className={classes.btn}>Login</button>
-            </div >
-  )
-}
