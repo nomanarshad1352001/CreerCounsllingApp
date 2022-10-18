e50447ff1b41671c5e38d8e502a1e028fa296bf8
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-10-18 09:23:33 -0700

    Delete Duplicate

--- src/Components/Components/UI/MyCard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Card.css';
-export default function MyCard(props) {
-  const classes = 'card ' + props.className;
-  
-  return <div className={classes}>{props.children}</div>;
-}
