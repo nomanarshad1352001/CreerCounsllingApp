9281d7f7a7289defc4e7e628a1e951de45d78ce4
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-11-01 11:46:14 +0500

    Footer Change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,33 +1,33 @@
 import React from "react";
 import classes from "./Footer.module.css";
-import {
-  FaFacebookSquare,
-  FaTwitterSquare,
-  FaInstagramSquare,
-} from "react-icons/fa";
+import { FiFacebook,
+         FiTwitter,
+         FiInstagram 
+        } from "react-icons/fi";
 const Footer = () => {
   return (
     <React.Fragment>
       <div className={classes.footer}>
         <section className={classes.container}>
+          <div className={classes.links}>
           <ul className={classes.list}>
-            <h4>GO To</h4>
+            <h3>Go To</h3>
             <li>Home</li>
             <br />
             <li>Contact</li>
             <br />
             <li>About</li>
           </ul>
-          <ul>
-            <h4>Institutions</h4>
+          <ul className={classes.list}>
+            <h3>Institutions</h3>
             <li>Schools</li>
             <br />
             <li>Colleges</li>
             <br />
             <li>University</li>
           </ul>
-          <ul>
-            <h4>Related Links</h4>
+          <ul className={classes.list}>
+            <h3>Related Links</h3>
             <li>
               <a href="/">Okas</a>{" "}
             </li>
@@ -42,26 +42,27 @@ const Footer = () => {
               <a href="/">Punjab university</a>
             </li>
           </ul>
+          </div>
           <div className={classes.social}>
-            <h4>Social Media</h4>
+            <h3>Social Media</h3>
             <a href="/">
               <i>
-                <FaFacebookSquare />
+                <FiFacebook />
               </i>
             </a>
             <a href="/">
               <i>
-                <FaTwitterSquare />
+                <FiTwitter />
               </i>
             </a>
             <a href="/">
               <i>
-                <FaInstagramSquare />
+                <FiInstagram />
               </i>
             </a>
           </div>
         </section>
-        <p className={classes.copyright}>Career counsling App @ 2022</p>
+        <p className={classes.copyright}>© 2022 Career & Degree Counselling App, All Rights Reserved</p>
       </div>
     </React.Fragment>
   );
