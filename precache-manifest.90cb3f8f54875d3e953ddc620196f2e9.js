be22e0663cea6e7f13b9a4de9af0602011025355
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-11-09 22:34:26 +0500

    Updates

---
+++ precache-manifest.90cb3f8f54875d3e953ddc620196f2e9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2505f82feca552003e9a",
+    "url": "/CreerCounsllingApp/static/css/main.0b368f5a.chunk.css"
+  },
+  {
+    "revision": "2505f82feca552003e9a",
+    "url": "/CreerCounsllingApp/static/js/main.2505f82f.chunk.js"
+  },
+  {
+    "revision": "2d281c672a566deb1a05",
+    "url": "/CreerCounsllingApp/static/js/1.2d281c67.chunk.js"
+  },
+  {
+    "revision": "1a5842d0ca8f77e6d4f8",
+    "url": "/CreerCounsllingApp/static/js/2.1a5842d0.chunk.js"
+  },
+  {
+    "revision": "10109437dfaabd9bdd89",
+    "url": "/CreerCounsllingApp/static/js/runtime~main.10109437.js"
+  },
+  {
+    "revision": "27913a5f3a9de3c5fa9de03f4c2cda74",
+    "url": "/CreerCounsllingApp/static/media/mcom.27913a5f.png"
+  },
+  {
+    "revision": "bc517792c2a3f7d6dd0412ef1ca80a9f",
+    "url": "/CreerCounsllingApp/static/media/7.bc517792.png"
+  },
+  {
+    "revision": "c34758589a7ad3920c2437bdb2f08611",
+    "url": "/CreerCounsllingApp/static/media/6.c3475858.png"
+  },
+  {
+    "revision": "e98cb6181a47c1bf076f088978236e60",
+    "url": "/CreerCounsllingApp/static/media/2.e98cb618.png"
+  },
+  {
+    "revision": "a68030d073eb9e53bedf915899974666",
+    "url": "/CreerCounsllingApp/static/media/asad.a68030d0.jpg"
+  },
+  {
+    "revision": "f1f2835e4c40ef4a3c7d09f604978a1e",
+    "url": "/CreerCounsllingApp/static/media/8.f1f2835e.png"
+  },
+  {
+    "revision": "fd9cdd8dfba34293ef95a60c37fa9f48",
+    "url": "/CreerCounsllingApp/static/media/9.fd9cdd8d.png"
+  },
+  {
+    "revision": "ce70afc7c141a801383c450b4d06e782",
+    "url": "/CreerCounsllingApp/static/media/4.ce70afc7.png"
+  },
+  {
+    "revision": "261f8dfa51c7556770bf4dd152f131a1",
+    "url": "/CreerCounsllingApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mainPage/precache-manifest.ff8343e1754b1f74908d5e7e1a53bb16.js"
+  "/CreerCounsllingApp/precache-manifest.90cb3f8f54875d3e953ddc620196f2e9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mainPage/index.html", {
+workbox.routing.registerNavigationRoute("/CreerCounsllingApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
