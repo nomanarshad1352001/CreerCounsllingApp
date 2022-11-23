cc575840ce8068894af35f032f385ef756f7ed18
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-11-23 12:03:44 +0500

    Updates

---
+++ precache-manifest.608608883f3ed02d2dfc0666be795705.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc7751e8cabf7b4826f9",
+    "url": "/CreerCounsllingApp/static/css/main.80433e0f.chunk.css"
+  },
+  {
+    "revision": "fc7751e8cabf7b4826f9",
+    "url": "/CreerCounsllingApp/static/js/main.fc7751e8.chunk.js"
+  },
+  {
+    "revision": "b3be8a9b1cc833812d75",
+    "url": "/CreerCounsllingApp/static/js/1.b3be8a9b.chunk.js"
+  },
+  {
+    "revision": "f976cf0471cb4a2809ad",
+    "url": "/CreerCounsllingApp/static/js/2.f976cf04.chunk.js"
+  },
+  {
+    "revision": "638b69e2baea2b28d76a",
+    "url": "/CreerCounsllingApp/static/js/runtime~main.638b69e2.js"
+  },
+  {
+    "revision": "27913a5f3a9de3c5fa9de03f4c2cda74",
+    "url": "/CreerCounsllingApp/static/media/mcom.27913a5f.png"
+  },
+  {
+    "revision": "4c21d1dba714aa17e885c3bc77b424cf",
+    "url": "/CreerCounsllingApp/static/media/inter.4c21d1db.png"
+  },
+  {
+    "revision": "e3afb3c38b359e5996fc8ed0e5951b5d",
+    "url": "/CreerCounsllingApp/static/media/masters.e3afb3c3.png"
+  },
+  {
+    "revision": "a68030d073eb9e53bedf915899974666",
+    "url": "/CreerCounsllingApp/static/media/asad.a68030d0.jpg"
+  },
+  {
+    "revision": "0917ca179d1b95585decdd08383b0b94",
+    "url": "/CreerCounsllingApp/static/media/civil.0917ca17.jpg"
+  },
+  {
+    "revision": "0b31a13c293f86ea226ff9a11de74aae",
+    "url": "/CreerCounsllingApp/static/media/phy.0b31a13c.png"
+  },
+  {
+    "revision": "4eaddc09a61d3bada02e00ec2f708495",
+    "url": "/CreerCounsllingApp/static/media/pre-eng.4eaddc09.jpg"
+  },
+  {
+    "revision": "977cd73192a3cddab7c3f25ef8798669",
+    "url": "/CreerCounsllingApp/static/media/no.977cd731.jpg"
+  },
+  {
+    "revision": "4e61608c9c6d11e95411a2ae212b4d65",
+    "url": "/CreerCounsllingApp/static/media/gc-town.4e61608c.jpg"
+  },
+  {
+    "revision": "b2a24583f21cff33acdafc6c1dcdfc34",
+    "url": "/CreerCounsllingApp/static/media/1.b2a24583.png"
+  },
+  {
+    "revision": "ba6f567ca418eb7dd3471840e0b4bca8",
+    "url": "/CreerCounsllingApp/static/media/2.ba6f567c.png"
+  },
+  {
+    "revision": "d3caaea3e395f0d4f59ec80bbf11cca7",
+    "url": "/CreerCounsllingApp/static/media/3.d3caaea3.png"
+  },
+  {
+    "revision": "b3dcb745f0234ea08c2b98cbb5c5324c",
+    "url": "/CreerCounsllingApp/static/media/4.b3dcb745.png"
+  },
+  {
+    "revision": "720d7db15bd605bdf39f6623bea6d4de",
+    "url": "/CreerCounsllingApp/static/media/careerCover.720d7db1.jpg"
+  },
+  {
+    "revision": "2450baf8905523fdd0189a91b31b8aa0",
+    "url": "/CreerCounsllingApp/static/media/log.2450baf8.png"
+  },
+  {
+    "revision": "c5dd01aae40e934a478f08b48f51a9fd",
+    "url": "/CreerCounsllingApp/static/media/img2-01.c5dd01aa.png"
+  },
+  {
+    "revision": "e06a34e7a1f4f0bfd684202b47ea5dd8",
+    "url": "/CreerCounsllingApp/index.html"
+  }
+];
